e01103d28323878b103203b5b71c52b24a784524
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-04-13 22:50:20 +0530

    Added Source code of Car-rental frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register/Register';
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
+import UserSignin from './components/Signin/userSignin';
+import {ToastContainer} from "react-toastify";
+import AgencySignin from './components/Signin/AgencySignin';
+import Header from './components/Header/Header';
+import AddCar from './components/Add cars/AddCars';
+import { useEffect } from 'react';
+import AvailableCars from './components/user/AvailableCars';
+import ViewRentedCars from './components/user/ViewRentedCars';
+import BookedCars from './components/Add cars/BookedCars';
+
+
+const Routing = () =>{
+  const navigate = useNavigate()
+  useEffect(()=>{
+    const agencyToken = localStorage.getItem("agencytoken")
+    const usertoken = localStorage.getItem("usertoken")
+    if(!agencyToken && !usertoken){
+      return navigate("/register")
+    }
+  },[])
+
+  return<>
+    <Header/>
+    <Routes>
+      <Route path='/register' element={<Register/>} />
+      <Route path='/usersignin' element={<UserSignin/>} />
+      <Route path='/agencysignin' element={<AgencySignin/>} />
+      <Route path='/addcar' element={<AddCar/>} />
+      <Route path='/viewcar' element={<AvailableCars/>} />
+      <Route path='/viewrentedcars' element={<ViewRentedCars/>} />
+      <Route path='/bookedcars' element={<BookedCars/>} />
+    </Routes>
+  </>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routing/>
+      <ToastContainer/>
+    </BrowserRouter>
   );
 }
 
