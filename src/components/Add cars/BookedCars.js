80ee42c7a804de3b9e94520fd8cd64ab246b2a22
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-04-18 17:25:10 +0530

    Added render url in Bookedcar component

--- src/components/Add cars/BookedCars.js
+++ src/components/Add cars/BookedCars.js
@@ -5,7 +5,7 @@ function BookedCars(){
 const [bookedCars,setBookedCars] = useState([])
 
     useEffect(()=>{
-        fetch("http://localhost:8080/bookedcars",{
+        fetch("https://car-rental-backend-fpn8.onrender.com/bookedcars",{
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("agencytoken"),
                 "Content-Type": "application/json"
